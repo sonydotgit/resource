00e36fbbf994c67ed341ed2dd91a2e5c93021b9f
sonydotgit <forhoooman@gmail.com>
2022-02-17 23:40:25 +0530

    add 18cs52

---
+++ 18cs52/mod1/images.pl
@@ -0,0 +1,20 @@
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# Associate images original text with physical files.
+
+
+$key = q//;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img2.svg"
+ ALT="\begin{figure}\centering
+\begin{tabular}{lll}
+\hline
+\textnormal{\textbf{Appl...
+...\\\\
+\hline
+File Transfer &amp; FTP &amp; TCP\\\\
+\hline
+\end{tabular}
+\end{figure}">;WIDTH="0" HEIGHT="0"|; 
+
+1;
+
